634480bfdac0fda016217d5307e983088fd0a0ed
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-14 12:39:17 -0700

    character print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * mian - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	char ch='a';
+	do
+	{
+		putchar (ch);
+		ch++;
+	}while (ch<'z');
+	putchar('\n');
+	return (0);
+}
