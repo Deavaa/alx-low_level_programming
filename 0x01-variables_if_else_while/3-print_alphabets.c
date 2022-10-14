61af5c69af643621b351dcbb34aa9531eec43df0
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-14 13:30:36 -0700

    capital and small letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char chsmall='a';
+	char chcaps='A';
+	do {
+		putchar (chsmall);
+		chsmall++;
+	} while (chsmall<='z');
+	do {
+		putchar (chcaps);
+		chcaps++;
+	} while (chcaps<='Z');
+	putchar('\n');
+	return (0);
+}
