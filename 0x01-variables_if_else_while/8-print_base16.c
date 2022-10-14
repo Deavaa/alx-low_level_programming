41e5a0bd7480a023aa69d592b7d1ebd674041630
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-14 14:34:37 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return:0 Always (Success)
+ */
+
+int main(void)
+{
+	char d = '0';
+
+	char ch = 'a';
+
+	while(d < 10)
+	{
+		putchar (d);
+		d++;
+	}
+	while(ch <= 'f')
+	{
+		putchar (ch);
+		ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
