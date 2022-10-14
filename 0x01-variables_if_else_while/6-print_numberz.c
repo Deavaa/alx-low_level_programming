ac4931c63bec1e5bd1bcb2fe424e8739f3b102cc
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-14 14:05:53 -0700

    puthcar nubmer

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point 
+ *
+ * Return:0 Alwasy (Sucess)
+ */
+
+int main (void)
+{
+	int x=0;
+	
+	while (x < 10)
+	{
+		putchar ( (char) x);
+		x++;
+	}
+	putcar ('\n');
+	return (0);
+}
