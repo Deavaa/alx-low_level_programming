8bf96acfb322b862f10d3539147fcd2b5254f695
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-14 11:54:59 -0700

    greater than five less than six and zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 		printf("Last digit of %d is %d and greater than 5\n", n, n%10);
 	if (n%10 == 0)
 		printf("Last digit of %d is %d and zero\n", n, n%10);
-	else if(n%10<6
+	else if(n%10<6)
 		printf("Last digit of %d is %d and is less than 6 and not zero\n", n, n%10);
 
 	return (0);
