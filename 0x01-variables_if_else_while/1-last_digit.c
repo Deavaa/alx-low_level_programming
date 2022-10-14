3a90f41c538d505e3c7143eec50940f0138bc3ad
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-14 11:53:02 -0700

    greater than five less than six and zero

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n%10 > 5)
+		printf("Last digit of %d is %d and greater than 5\n", n, n%10);
+	if (n%10 == 0)
+		printf("Last digit of %d is %d and zero\n", n, n%10);
+	else if(n%10<6
+		printf("Last digit of %d is %d and is less than 6 and not zero\n", n, n%10);
+
+	return (0);
+}
+
