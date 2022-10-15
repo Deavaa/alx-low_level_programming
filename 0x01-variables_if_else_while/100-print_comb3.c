269553d7511e981cdf440f3ee8d377aad6128085
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-15 13:14:01 -0700

    combination of number char

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int i, j;
 
-	for ( i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-	for( j = '0'; j <= '9';j++)
+	for(j = '0'; j <= '9';j++)
 		{
 		if(i < j)
 		{
 
 		putchar (i);
 		putchar (j);
-		if( !( i == '8' && j == '9'))
+		if(!( i == '8' && j == '9'))
 		{
 		putchar (',');
 	putchar(' ');
