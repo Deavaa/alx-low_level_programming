378e4e9dbfdfadb5cc289dee6514ac452d39e25c
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-16 12:23:10 -0700

    two numbers two digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * mian - Entry Point
+ * main - Entry Point
  *
  * Return:0 Always (Success)
  */
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i, j;
-	
+
 	for (i = 0; i <= 98; i++)
 	{
 		for (j = 0; j <= 99; j++)
