e8cc3711c66f99a1ee5e1d4d536c7bf9662a46f9
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-16 02:16:43 -0700

    three deight character

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry Point
+ *int x,y,z;
+ for (x = '0'; x <= '9'; x++)
+ {
+ for (y = '0'; y <= '9'; y++)
+ {
+ for (z = '0'; z <= '9'; z++)
+ {
+  if (x <= '7' && y <= '8' && z <= '9')
+  {
+  putchar (x);
+  putchar (y);
+  putchar (z);
+  if (!(x == '7' &&  && y == '8' && z == '9'))
+  {
+  putchar (',');
+  putchar (' ');
+  }
+  }
+  }
+  }
+  }
+  putchar('\n');
+  }
+  
