9f4c318c57cf82c46ac2e9cf598af100f4face32
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-16 02:46:50 -0700

    three characters

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,26 @@
  */
 int main(void)
 {
-int x,y,z;
- for (x = '0'; x <= '9'; x++)
- {
- for (y = '0'; y <= '9'; y++)
- {
- for (z = '0'; z <= '9'; z++)
- {
-  if (x <= '7' && y <= '8' && z <= '9')
-  {
-  putchar (x);
-  putchar (y);
-  putchar (z);
-  if (!(x == '7' &&  && y == '8' && z == '9'))
-  {
-  putchar (',');
-  putchar (' ');
-  }
-  }
-  }
-  }
-  }
-  putchar('\n');
-  }
-  
+	int x,y,z;
+
+	for (x == '0'; x <= '7'; x++)
+	{
+		for (y =='0'; y <= '8'; y++)
+		{
+			for (z == '0'; z <= '9'; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(y);
+				if(!(x == '7' && y == '8'; && z == '9'))
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+}
+		
+
