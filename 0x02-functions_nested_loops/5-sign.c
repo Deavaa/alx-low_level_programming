cbe1aacf13406a9a68f78df6fdefa6f8b5aa5d24
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-18 13:16:37 -0700

    print sing of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sing of number
+ * @n: bring the value to the function
+ * Return: 1 and print + if n is greater than zero
+ * Return: 0 and print 0 if n is zero
+ * Return: -1 and print - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else{
+		_putchar ('0');
+		return (0)
+	}
+}
