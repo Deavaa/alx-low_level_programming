188d3fd7dd6726fff332a3505fc3c6086451eebe
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-18 13:23:38 -0700

    print sing of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign - print the sing of number
  * @n: bring the value to the function
- * Return: 1 and print + if n is greater than zero, 
- * Return 0 and print 0 if n is zero, 
- * Return -1 and print - if n is less than zero
+ * Return: 1 and print + if n is greater than 0
+ * Return 0 and print 0 if n is zero
+ * Return -1 and print - if n is less than 0
  */
 
 int print_sign(int n)
