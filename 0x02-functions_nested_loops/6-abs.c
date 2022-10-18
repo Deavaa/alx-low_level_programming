9d0f516d0e1352ea13b7e6bfd1504a3b6efc929d
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-18 13:31:58 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @r :brings the value to the function
+ * Return:0 
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r = r * -1;
+	}
+	printf ("%d\n",r);
+	return 0;
+}
