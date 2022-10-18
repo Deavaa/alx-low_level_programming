6e306fe95c54a1899ffd3ef1cfea174a5267e5f3
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-18 12:43:36 -0700

    print alpahbet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ *
  * print_alphabet_x10 return void
  *
  * Return noting
@@ -10,7 +11,7 @@ void print_alphabet_x10(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		char ch = 'a';
 
