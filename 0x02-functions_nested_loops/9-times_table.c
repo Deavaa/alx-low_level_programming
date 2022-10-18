4c82ebed7a21ca852d5bf3f9ad2a5843897cd3ab
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-18 14:32:38 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,21 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, res;
 
 	for (i = 0; i <= 9 ; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar (i * j + '0');
+			res = i * j ;
+			if (res < 10)
+			{
+			_putchar (res + '0');
+			}
+			else
+			{
+				_putchar (res / 10);
+				_putchar (res % 10);
 			if (j != 9)
 			{
 				_putchar (',');
