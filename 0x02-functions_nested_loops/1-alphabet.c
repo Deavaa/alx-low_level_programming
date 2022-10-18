cc2e63af973319c4f20ee900ca3d5a003d00f0fd
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-18 11:56:15 -0700

    lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return:0 Always (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar (ch);
+		ch++;
+	}
+	_putchar ('\n');
+	return (0)
+}
+
