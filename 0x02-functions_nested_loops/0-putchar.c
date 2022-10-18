8f4f4f18b0ad1f65c2975b6c425965799bef7d22
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-18 11:30:24 -0700

    puthcar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,18 @@
 /**
  * main - Entry Point
  *
- * Return Always :0 (Success)
+ * Return Always : 0 (Success)
  */
 
-int main(void)
+int mian(void)
 {
-	char [] var="_putchar";int i;
-	for(i =0;i <= 7; i++)
+	char [] var="_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
 	{
-		putchar(var[i];
-				}
-				putchar('\n";
-					return (0);
-					}
+		putchar (var[i]);
+	}
+	putchar ('\n');
+	return (0);
+}
