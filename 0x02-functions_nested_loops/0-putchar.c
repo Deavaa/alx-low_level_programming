67457853569739132e998284653c74886cd852c6
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-18 11:16:05 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return Always :0 (Success)
+ */
+
+int main(void)
+{
+	char [] var="_putchar";int i;
+	for(i =0;i <= 7; i++)
+	{
+		putchar(var[i];
+				}
+				putchar('\n";
+					return (0);
+					}
