742b1ab97750e3b6eecb91cb63f1eb366e4eb9b1
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-24 13:31:23 -0700

    reverse printing the text

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - print the string characters n reverse order
- * @s birng the original string
+ * @s: birng the original string
  */
 
 void print_rev(char *s)
@@ -14,7 +14,7 @@ void print_rev(char *s)
 	{
 		len++;
 	}
-	for (i = len-1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar (s[i]);
 	}
