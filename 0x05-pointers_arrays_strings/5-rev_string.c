f0bc396ff5898ace107ea763c760e8f013a7e73f
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-24 14:00:04 -0700

    revrse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,17 @@ void rev_string(char *s)
 {
 	int len = 0;
 	int i, j;
+	char tmp1, tmp2;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	if (len != 0)
+	for (i = len - 1, j = 0; i > j; i--, j++)
 	{
-	char *ch = {[len] = 0};
-
-	for (i = len - 1, j = 0; i >= 0; i--, j++)
-	{
-		ch[i] = s[j];
-	}
-	s = ch;
+		tmp1 = s[i];
+		tmp2 = s[j];
+		s[i] = tmp2;
+		s[j] = tmp1;
 	}
 }
