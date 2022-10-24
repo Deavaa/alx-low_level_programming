abb097b41b66ea61415b52152d5836ebd541166d
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-24 14:49:51 -0700

    n number of elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - print n elments of arry of integers
+ * @a: brings the array to the funciton
+ * @n: is the number of elements to be printerd
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i =0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n-1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
