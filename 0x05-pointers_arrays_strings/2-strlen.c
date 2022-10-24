7aff7330ffa934ff81d357cd3fc35378b622fe17
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-24 12:21:30 -0700

    count the character in the sting

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - compute the lnegth of a sting
+ * @s :bring the string to the funciton
+ * Returns : the length of the sting
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+	for (i = 0; i != "\0"; i++)
+	{
+		len++;
+	}
+	return (len);
+}
