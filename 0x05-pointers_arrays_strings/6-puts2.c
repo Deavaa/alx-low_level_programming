4381d9d96ec5d8258b19ebc841b0764de94c0099
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-24 14:10:10 -0700

    print every character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,18 +2,18 @@
 
 /**
  * puts2 - prints every other character
- * @s: brings the string
+ * @str: brings the string
  */
 
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (1 % 2 == 0)
 		{
-			_putchar (s[i]);
+			_putchar (str[i]);
 		}
 	}
 	_putchar ('\n');
