6aa6c7bf572d3c45dcf44b71997ee817119f731e
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-24 11:38:51 -0700

    upaudate the value it points

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- *
  * reset_to_98 - updates the value it points to 98
- * @n : the e value in memor to be modify
+ * @n : the  value in memor to be modify
  */
 
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
 
