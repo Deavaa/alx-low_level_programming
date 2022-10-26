ac3cc5c050f1eca45ae496ac77773965e2532db8
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-26 11:25:22 -0700

    text or string concatination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatinate two strings
+ * @dest: destination pointer
+ * @src: source pointer
+ * Return: the reresult
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
