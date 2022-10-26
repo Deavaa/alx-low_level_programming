a4259f0bb80b43659195594fde0fbde36d5e47b8
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-26 12:52:48 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,14 +4,14 @@
  * _strncpy - copy string from one var to another
  * @dest: destination var
  * @src: te soucrce string
- * @n: number of characters to be copy
+ * @n: number of ch aracters to be copy
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0;i < n; i++)
+	fori (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
