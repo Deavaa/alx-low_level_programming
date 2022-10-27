8630870380ecbbc44287c937d3defc2ea05f679f
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-27 12:26:17 -0700

    convert to number

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - convert letters to numers
+ * @ch: brings the string
+ * Return: reuslt
+ */
+
+char *leet(char *ch)
+{
+	int i, j;
+	char *li = "aAeEoOtTlL";
+	char *num = "4433007711";
+
+	for(i = 0; ch[i] != '\0'; i++)
+		for(j = 0; li[j] != '\0'; j++)
+		{
+			if (ch[i] == li[j])
+			{
+				ch[i] = num[j];
+			}
+		}
+}
