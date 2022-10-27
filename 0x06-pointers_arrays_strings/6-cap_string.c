391b71bdc6b34cbbb56491a2405d43a86718d896
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-27 11:35:10 -0700

    convert each word first letter to upper

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,16 +10,17 @@
 char *cap_string(char *ch)
 {
 	int i;
+	char *res;
 
 	for (i = 0; ch[i] != '\0'; i++)
 	{
 		if (ch[i] >= 97 && ch[i] <= 122 && (ch[i - 1] == '\n' || ch[i - 1] =='\t' || ch[i - 1] == ' ' || ch[i - 1] == ',' || ch[i - 1] == ';' || ch[i - 1] == '.' || ch[i - 1] == '!', ch[i - 1] == '?' || ch[i - 1] == '"' ch[i - 1] == '(' || ch[i - 1] == ')' || ch[i - 1] == '{' || ch[i - 1] == '}' || i == 0))
 		{
-			ch[i] = ch[i] - 32;
+			res[i] = ch[i] - 32;
 		}
 		else
 		{
-			ch[i] = ch[i];
+			res[i] = ch[i];
 	}
-	return (ch);
+	return (res);
 }
