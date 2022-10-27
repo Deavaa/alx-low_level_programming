b2ae8d903c1bfd9481a9520234ccdd56674f8622
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-27 01:29:32 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @dest: destination var
  * @src: te soucrce string
  * @n: number of characters to be copy
+ * Return: the destination vvariable
  */
 
 char *_strncpy(char *dest, char *src, int n)
