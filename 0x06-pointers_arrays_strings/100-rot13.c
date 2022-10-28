4f7ddfa7bf5e0db0e49a6f287c01f0cfa55670d7
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-28 00:31:51 -0700

    rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,7 +11,7 @@ char *rot13(char *ch)
 	int i;
 	for (i = 0; ch[i] != '\0' && ((ch[i] >= 'a' && ch[i] <= 'z') || (ch[i] >= 'A' && ch[i] <= 'Z')); i++)
 	{
-			if((ch[i] >= 'a' && ch[i] <= 'm') || (ch[i] >= 'A' && ch[i] <= 'M'))
+			if ((ch[i] >= 'a' && ch[i] <= 'm') || ch[i] <= 'M')
 			{
 				ch[i] += 13;
 			}
