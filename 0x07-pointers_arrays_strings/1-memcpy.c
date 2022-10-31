824aab27aae768b152ad5e0477f9283e35ffc1df
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-10-31 11:12:44 -0700

    copy pointer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy -copies memory area
+ * @dest: destinantion pointer
+ * #src: source pointer
+ * @n: bytes
+ * Retern: destination pointer
+ */
+
+char *_memcpy(char * dest, char *src, int n)
+{
+	for (; n > 0; n--)
+	{
+		dest[n-1] = src[n-1];
+	}
+	return (dest);
+}
