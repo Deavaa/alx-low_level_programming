9284b77a575d03915ea03dee6b831e0db6dfabcf
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-01 11:02:58 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn -get the lnegeth
+ * @s: where
+ * @accept: acceoted cgars
+ * Return: length of occurce
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char txt = accept;
+	unsigned int i = 0;
+
+	while (*s++)
+	{
+		while (*accept++)
+		{
+			if (*(--s) == *(--accept))
+			{
+				c++;
+				break;
+			}
+			if (!(*--acept))
+			{
+				break;
+				accept = ttxt;
+			}
+			return (c);
+		}
+	}
+}
