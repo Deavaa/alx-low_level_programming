2e0f4bfb0acac5f88182645d55e34eb661651436
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-01 12:07:13 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+nclude "main.h"
+
+/**
+ * _strpbrk -  locates the first occurrence
+ * @s: string
+ * @accept: bytes in the string
+ * Return: the byte in *s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (0);
+}
