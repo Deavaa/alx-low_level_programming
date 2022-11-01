cc5566fc6f75b9dffc256cf3b940a53f290f4f64
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-01 14:28:35 -0700

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboar - print chessboard
+ * @a: dimension
+ * Return: noting
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	char *ch = {{'r','k','b','q','n','k','b','r'},{'p','p','p','p','p','p','p','p'},{' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' '}};
+	int j, k;
+
+	for (j = 0; j < 4; j++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			_putchar (ch[j][k]);
+			if (k != 7)
+			{
+			_putchar (',');
+			}
+
+		}
+	}
+	for (j = 3; j >= 0; j--)
+	{
+		for (k = 7; k >= 0; k--)
+		{
+			if ((ch[j][k] >= 'a' || ch[j][k] <= 'm') && ch[j][k] != ' ')
+			{
+				_putchar (ch[j][k] + 13);
+			}
+			else if ((ch[j][k] > 'm') && ch[j][k] != ' ')
+			{
+				_putchar (ch[j][k] - 13);
+			}
+			else
+			{
+				_putchar (ch[j][k]);
+			}
+			if (k != 0)
+			{
+				_putchar (',');
+			}
+		}
+	}
+}
+
