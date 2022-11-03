528c13d7bba2266c9ace92e0717fda11ba488b2a
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-03 10:27:10 -0700

    power of nuber

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power result of two number 
+ * @x: base value
+ * @y: power value
+ * Return: the power result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y -1));
+}
