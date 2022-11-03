3f8af198ca607daf58d5ea116df5f71c3dc84b4d
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-03 10:51:11 -0700

    sqr

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,3 +16,11 @@ int _sqrt_recursion(int n, int val)
 	else
 		return (_sqrt_recursion(n, val + 1));
 }
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (_sqrt_recursion(n, 1));
+}
