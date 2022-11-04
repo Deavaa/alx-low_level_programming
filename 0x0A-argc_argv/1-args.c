27ba9e2811b8761dfc861073363f603581ecb331
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-04 10:11:58 -0700

    coutn

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "stdlib.h>"
+
+/**
+ * main - prints no. of arguments
+ * @argc: 1st param
+ * @argv: 2nd param
+ * Return: 0
+ */
+
+int main( int argc, char **argv)
+{
+	int val;
+
+	for(val = 0; val < argc; val++)
+	{
+		*argv[val] = *argv[val];
+	}
+	printf(%d\n, (sum - 1));
+	return (0);
+}
