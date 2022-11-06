7c7212f67941293d44f576373d3d0a748edbad4a
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-06 01:27:56 -0700

    product

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplication of two numbers
+ * @argc: no args
+ * @argv: array of pointers
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2, res;
+	if (argc != 3)
+	{
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	res = n1 * n2;
+	printf("%d\n", res);
+	return (0);
+}
