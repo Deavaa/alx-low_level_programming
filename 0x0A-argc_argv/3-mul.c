85e6908560c4339a8b7f4e87d156abe9adbcf4c9
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-06 01:47:17 -0700

    product

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int n1, n2, res;
-	
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -20,6 +20,7 @@ int main(int argc, char *argv[])
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[2]);
 	res = n1 * n2;
+
 	printf("%d\n", res);
 	return (0);
 }
