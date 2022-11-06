91a17477dda0edcc124944548bfad8c984800410
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-06 01:33:45 -0800

    adds

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,14 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i;
 	int res = 0;
 	
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+		if (atoi(argv[i]) < '0' || atoi(argv[i]) > '9')
 			{
 				printf("Error\n");
 				return (1);
