010b844e14f5b36cf8dc9f992105f168f393d9a8
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-06 01:13:26 -0700

    all argmts

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - prints all argu
+ * @argc: 1st param
+ * @argv: 2nd param
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
