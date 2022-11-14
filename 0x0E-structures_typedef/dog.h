af61c123489d0f1116191db72d1dec9a4eece35a
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-14 10:12:28 -0800

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines the dog attributes
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of thedog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
