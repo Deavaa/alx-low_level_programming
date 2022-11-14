04235599918cd6682d6a413b1234861dcccfb915
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-14 10:24:50 -0800

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *dg, char * name, float age, char *owner);
+void init_dog(struct dog *dg, char *name, float age, char *owner);
 void print_dog(struct dog *dg);
 typedef struct dog dg_type;
 dg_type *new_dog(char *name, float age, char *owner);
