e2249ac83d05c539e2e728d99c75ecbb7ffaa42a
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-17 08:34:24 -0800

    print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/***
+ * print_name - prints a name
+ * @name: input name
+ * @f: points to the function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
