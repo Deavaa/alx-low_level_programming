83598e8043be333e980cc4e726acdb54bf4591c9
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-23 23:01:28 -0800

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "l_list.h"
+#include <stdio.h>
+
+void first(void) __attribute__((constructor));
+/**
+ * first - print sentence before  the main
+ * Return: nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
