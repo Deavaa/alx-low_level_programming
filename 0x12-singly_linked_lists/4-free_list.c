9cdf30b1c033453765d6f79dd67d97c1fad6c183
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-23 22:50:00 -0800

    free the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "l_list.h"
+
+/**
+ * free_list - free the list
+ * @head: head node of the list to be free
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while(head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
