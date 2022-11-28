48f85400cf2415981c9f93092a39180ea294424b
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-28 03:47:02 -0800

    number of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - no of elements
+ * @h: pointer to list
+ * Return: no of list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *tmp;
+	int = 0;
+
+	tmp = h;
+	while (tp)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
