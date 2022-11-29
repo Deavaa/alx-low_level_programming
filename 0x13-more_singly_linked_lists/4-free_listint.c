dbe26d8229f626984035201b0deca5d396da2b7b
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-29 03:25:45 -0800

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint - free the linked list
+ * @head: pointer to head node
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *cur,*tmp;
+	cur = head;
+	while (cur != NULL)
+	{
+		tmp = cur->next;
+		free(cur);
+		cur = tmp;
+	}
