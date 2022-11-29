4e64735b13e4f028b2722954d556199d5a79563a
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-29 04:09:56 -0800

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *tmp;
 	unsigned int i = 0;
 
-	tmp = h;
-	while (tmp)
+	while (h)
 	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		i++;
 	}
 	return (i);
