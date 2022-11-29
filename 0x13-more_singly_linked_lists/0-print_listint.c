92b82f82bfcf08588ec1683ab8b79c357542f6bc
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-29 09:07:49 -0800

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num_of_nodes = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num_of_nodes++;
+		i++;
 	}
-	return (num_of_nodes);
+	return (i);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,13 +1,12 @@
-#ifndef LISTS_H
-#define LISTS_H
-#include <string.h>
-#include <Stdio.h>
+#ifndef LIST_H_
+#define LIST_H_
+#include <stdlib.h>
 #include <unistd.h>
 
 /**
- * struct listint_s - single linked list
- * @n: int
- * @next: pointer to next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  */
 
 typedef struct listint_s
@@ -28,7 +27,5 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
+
 #endif
