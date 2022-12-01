2c36134303cf47df8419827d8d1c6eef7d5c858e
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-01 09:30:46 -0800

    decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_binary - decial to binary
+ * @n: decimal number
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	int j = 1;
+
+	j <<= 63;
+	if (n > 0)
+	{
+		_putchar('0');
+	}
+	while (j > 0)
+	{
+		if ((n & j) == 0 && i ==1)
+		{
+			_putchar('0');
+		}
+		else if ((n & j) != 0)
+		{
+			_putchar('1');
+			i = 1;
+		}
+		j = j >> 1;
+	}
+}
