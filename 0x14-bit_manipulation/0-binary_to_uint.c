22a5fbdd0fd0a3dc1f948b381c668406612a8f4f
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-01 08:50:53 -0800

    convert binary nuber to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: binary no.
+ * Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int n = 0;
+	int i, j;
+
+	if (b ==NULL)
+	{
+		return (0);
+	}
+	for (i = 0; s[i]; i++)
+	{
+		;
+	}
+	for (j = 0; j != len; j++)
+	{
+		if(b[i - j - 1] == '1')
+		{
+			n += 1 << j;
+		}
+		else if(b[i - j - 1] != '0')
+		{
+			return (0);
+		}
+	}
+	return (n);
+}
