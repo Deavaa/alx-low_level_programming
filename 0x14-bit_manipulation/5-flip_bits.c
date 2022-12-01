94099995e6430d6460196d023e797a31ee07b6b1
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-01 09:56:18 -0800

    count to filip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - the number of bits need to flip to get from one number to another
+ * @n: no.
+ * @m: compare
+ * Return: no.  diff
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i = 0;
+
+	n = n ^ m;
+	while (n)
+	{
+		i += n & 1;
+		n >>= 1;
+	}
+	return (i);
+}
