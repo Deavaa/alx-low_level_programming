126f62c4840f0cc7b8f1d47fa50042f83732fa55
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-01 10:16:48 -0800

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 1 little 0 big indian
+ */
+
+int get_endianness(void)
+{
+	int = 1;
+	char *ch = (char *)&i;
+
+	if (*ch)
+	{
+		return (1);
+	}
+	return (0);
+}
