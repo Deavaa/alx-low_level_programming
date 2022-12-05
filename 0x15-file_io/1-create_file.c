e7dda56362f91924fbb906f3be52462cf008912c
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-05 09:32:43 -0800

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+include "main.h"
+
+/**
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ * Return: 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i, j, k = 0;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (i < 0)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		while (text_content[y])
+		{
+			i++;
+		}
+		j = write(i, text_content, k);
+		if (j != k)
+		{			
+			return (-1);
+		}
+	}
+	close(i);
+	return (1);
+}
