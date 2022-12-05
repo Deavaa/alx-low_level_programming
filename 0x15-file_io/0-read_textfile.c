f96182c7d5ce671331f4f45f6056809d1345152f
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-05 09:00:28 -0800

    read and display

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * ssize - read and print file
+ * filename: file name
+ * letters: no. of letters
+ * Return: letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i, j, k;
+	char *buf;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	i = open(filename, O_RDONLY);
+	if (i < 0)
+	{
+		return (0);
+	}
+	buf = malloc(sizeof(char) * letters);
+	if(!buf)
+	{
+		return (0);
+	}
+	j = read(fd, buf, letters);
+	if (j < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[j] = '\0';
+	close(fd);
+	k = write(STDOUT_FILENO, buf, j);
+	if (k < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return(y);
+}
