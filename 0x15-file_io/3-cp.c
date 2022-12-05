3b4bf20470bf90797d197bd0167cb0eb252f87ec
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-12-05 10:22:14 -0800

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,11 +5,12 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	int fd_r, fd_w, x, m, n;
 	char buf[BUFSIZ];
-	
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -47,7 +48,8 @@ int main(int argc, char *argv[])
 		if (n < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
-			exit(100);
 		}
-		return (0);
+		exit(100);
+	}
+	return (0);
 }
